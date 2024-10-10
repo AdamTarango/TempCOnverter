86538cd76ff18375e0130e7da91a6e6acb49c49c
adam tarango <you@example.comadam.tarango97@gmail.comgit commitgit statusgit statusgit config --global user.email you@example.comadam.tarango97@gmail.com>
2024-10-10 10:35:08 -0700

    initial commit

---
+++ index.js
@@ -0,0 +1,28 @@
+const convertToCelsius = (farTemp) => {
+  Number (farTemp);
+  celTemp = (farTemp - 32) * (5/9);
+  return celTemp;
+}
+const describeTemp = (fartemp) => {
+  Number(fartemp);
+  if (fartemp < 32){
+    return `very cold`;
+  }
+  else if (fartemp < 64){ 
+    return `cold`;
+  }
+  else if (fartemp < 86){ 
+    return `warm`;
+  }  
+  else if (fartemp < 100){ 
+    return `hot`;
+  }
+  else if (true){
+    return `very hot`;
+
+  }
+
+}
+const temp = prompt("Input a farenheight temperature")
+alert(convertToCelsius (temp))
+alert(describeTemp (temp))
